88ef5a4b3f64da3e7905810379665c4262fae7a2
wironjr <jun1n_@live.com>
2022-09-25 16:40:46 -0300

    validaçao de valor

--- script.js
+++ script.js
@@ -2,25 +2,34 @@ function teste(event){
     event.preventDefault()
     let gasolinaInput = document.getElementById("gasolina-input").value
     let alcoolInput = document.getElementById("alcool-input").value
-  
     let resultText = document.getElementById("result-text")
     let textGasolina = document.getElementById("text-gasolina")
     let textAlcool = document.getElementById("text-alcool")
     let resultCard = document.getElementById("result-card")
 
+    let msgVerificaGas = document.getElementById("verifica-gasolina")
+    let msgVerificaAlcool = document.getElementById("verifica-alcool")
+
     //dividir o valor da gasolina pelo alcool, se de maior que 0,7 vale gasolina s der menor vale alcool
+    
     let result = alcoolInput / gasolinaInput
 
-    if (result > 0.7) {
-       
-        resultText.innerHTML = "Melhor usar Gasolina"
+    if((gasolinaInput == "") || (alcoolInput == "")){
+        msgVerificaAlcool.classList.remove("hide")
     }else{
-        
-        resultText.innerHTML = "Melhor usar Álcool"
+        if (result > 0.7) {
+       
+            resultText.innerHTML = "Melhor usar Gasolina"
+        }else{
+            
+            resultText.innerHTML = "Melhor usar Álcool"
+        }
+        resultCard.classList.remove("hide")
+        msgVerificaAlcool.classList.add("hide")
     }
 
     textGasolina.innerHTML = "Valor da gasolina R$ " + gasolinaInput
     textAlcool.innerHTML = "Valor do álcool R$ " + alcoolInput
 
-    resultCard.classList.remove("hide")
+   
 }
